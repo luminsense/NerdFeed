192d66f41e85a9ce40275ccbbd6ae28fb23d9d3f
luminsense <wulianglumin@foxmail.com>
2014-07-31 16:13:44 +0800

    initial commit
    project created

---
+++ NerdFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NerdFeed
+//
+//  Created by Lumi on 14-7-31.
+//  Copyright (c) 2014年 LumiNg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
