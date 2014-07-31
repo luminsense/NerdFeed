09094a23985ed2a2d35b1484438fef16f76f2af6
luminsense <wulianglumin@foxmail.com>
2014-07-31 16:16:23 +0800

    created table view controller

---
+++ LNGCoursesViewController.h
@@ -0,0 +1,13 @@
+//
+//  LNGCoursesViewController.h
+//  NerdFeed
+//
+//  Created by Lumi on 14-7-31.
+//  Copyright (c) 2014年 LumiNg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LNGCoursesViewController : UITableViewController
+
+@end
